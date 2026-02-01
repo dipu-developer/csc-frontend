adafb45153823ebde74ef663e35e937ca42fcde0
Mayur1708mali <Mayur1708mali@gmail.com>
2026-02-01 21:26:00 +0530

    lists product from backend

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import PaymentComponent from "./PaymentComponent";
 
 export default function ProductCard({
-	productName = "Laptop",
-	description = "High-performance laptop for professionals lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-	price = "$100",
-	amount = 100,
-	currency = "INR",
+	image,
+	productName,
+	description,
+	price,
+	amount,
+	currency,
 }) {
 	return (
-		<div className="m-4 bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition duration-200 w-full max-w-sm">
+		<div className="m-2 bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition duration-200 w-full border-2 border-gray-400 h-110 max-w-sm">
+			<div className="h-48 m-1 rounded-lg overflow-hidden">
+				<img
+					src={image}
+					alt={productName}
+					className="w-full h-full object-cover"
+				/>
+			</div>
 			{/* Card Content */}
 			<div className="p-4 flex flex-col h-64">
 				{/* Product Name */}
@@ -18,22 +26,24 @@ export default function ProductCard({
 				</h3>
 
 				{/* Description */}
-				<p className="text-gray-600 text-sm flex-1 mb-4">
-					{description}
+				<p className="text-gray-600 text-sm h-8 flex-1 mb-4">
+					{description?.length > 100
+						? `${description.substring(0, 100)}...`
+						: description}
 				</p>
-			</div>
-			<div className="p-4">
-				{/* Bottom Section: Price and Buy Button */}
-				<div className="flex justify-between items-center space-x-4 border-t pt-4">
-					<span className="text-xl font-bold text-blue-600">
-						{price}
-					</span>
-					<PaymentComponent
-						productName={productName}
-						description={description}
-						amount={amount}
-						currency={currency}
-					/>
+				<div className="py-4">
+					{/* Bottom Section: Price and Buy Button */}
+					<div className="flex justify-between items-center space-x-4 border-t pt-4">
+						<span className="text-xl font-bold text-blue-600">
+							{price}
+						</span>
+						<PaymentComponent
+							productName={productName}
+							description={description}
+							amount={amount}
+							currency={currency}
+						/>
+					</div>
 				</div>
 			</div>
 		</div>
