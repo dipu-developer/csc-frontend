3688cc624a8934b1d0ab9f4aedb819c50926ae49
Mayur1708mali <Mayur1708mali@gmail.com>
2026-02-07 16:46:17 +0530

    Added token verification

--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -1,8 +1,20 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 export default function PrivateRoute({ children }) {
 	const authUser = localStorage.getItem("authUser");
 	if (!authUser) return <Navigate to="/login" replace />;
+	const location = useLocation();
+
+	const getCookie = (name) => {
+		const value = `; ${document.cookie}`;
+		const parts = value.split(`; ${name}=`);
+		if (parts.length === 2) return parts.pop().split(";").shift();
+		return null;
+	};
+
+	const token = getCookie("authToken");
+	if (!token)
+		return <Navigate to="/login" state={{ from: location }} replace />;
 	return children;
 }
